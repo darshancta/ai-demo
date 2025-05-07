2f846c3d635380f4baa52dd5850c579c4f48bebe
DARASHAN M <darshancta@gmail.com>
2025-05-08 00:17:52 +0530

    Prompt template

--- src/main/java/com/example/ai/demo/HelloController.java
+++ src/main/java/com/example/ai/demo/HelloController.java
@@ -2,9 +2,14 @@ package com.example.ai.demo;
 
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HelloController {
@@ -17,6 +22,23 @@ public class HelloController {
 
     @GetMapping
     public String prompt(@RequestParam String message) {
+
         return chatClient.prompt(message).call().content();
+    }
+
+    //Api with Prompt Template
+    @GetMapping("/celebrate")
+    public String getCelebrate(@RequestParam String name) {
+
+        String message = "List the details of the Famous pernality {name} along with carrier achievments. Show the details in the readable formate";
+
+        PromptTemplate promptTemplate = new PromptTemplate(message);
+
+        Map map = new HashMap<String, String>();
+        map.put("name", name);
+
+        Prompt prompt = promptTemplate.create(map);
+
+        return chatClient.prompt(prompt).call().content();
     }
 }
